4b6458672e93fa520c55b8432a03066d0f17840e
clayton7000 <80071108+clayton7000@users.noreply.github.com>
2026-01-19 19:19:41 -0300

    Estudo de funções para string

---
+++ 5 - exercicio de manipulação de string.php
@@ -0,0 +1,14 @@
+<?php
+//função para localizar posição de uma palavra
+echo strpos("Hello world!", "world \n");
+
+$x = "Hello World! \n";
+echo strtoupper($x);
+
+$x = "Hello World! \n";
+echo strtolower($x);
+
+$x = "Hello World! \n";
+echo str_replace("World", "Dolly", $x);
+
+?>
