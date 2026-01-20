8b840fbb8f727dec919f6d5ab0bfb5c0c506322e
clayton7000 <80071108+clayton7000@users.noreply.github.com>
2026-01-20 19:31:50 -0300

    pequeno estudo do switch

---
+++ 7 - exercicio com switch.php
@@ -0,0 +1,29 @@
+<?php
+$dia=4;
+switch($dia){
+    case 1:
+        print("Today is Sunday");
+        break;
+    case 2:
+        print("Today is Monday");
+        break;
+    case 3:
+        print("Today is Tuesday");
+        break;
+    case 4:
+        print("Today is Wednesday");
+        break;
+    case 5:
+        print("Today is Thursday");
+        break;
+    case 6:
+        print("Today is Friday");
+        break;
+    case 7:
+        print("Today is Saturday");
+        break;
+    default:
+        print("option invalid");
+        break;
+}
+?>
